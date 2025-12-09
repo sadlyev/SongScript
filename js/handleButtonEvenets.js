91ab79e975bd368a82df0ed7599a597e6360abe9
Asilbek Sadiyev <201261859+dilo2222@users.noreply.github.com>
2025-12-10 00:59:28 +0500

    main functionality is ready

--- js/handleButtonEvenets.js
+++ js/handleButtonEvenets.js
@@ -1,1 +1,29 @@
+export function playSong() {
+    const cards = document.querySelectorAll(".music_item") 
 
+    let currentSong = null
+
+    cards.forEach((card) =>{
+        card.addEventListener("click", function(e) {
+        let song = e.target.closest(".music_card-audio")
+
+        if (currentSong === song) {
+            if (song.paused) {
+                song.play()
+            } else {
+                song.pause()
+                return
+            }
+        }
+
+        if (currentSong) {
+            currentSong.pause()
+            currentSong.currentTime = 0
+        }
+
+        song.play()
+        currentSong = song
+
+    })
+    })
+}
