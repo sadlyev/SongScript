66561662ce412b220cd677200f0995f137a2a137
Asilbek Sadiyev <201261859+dilo2222@users.noreply.github.com>
2025-12-17 14:23:08 +0500

    added like button

--- js/CardClass.js
+++ js/CardClass.js
@@ -1,17 +1,18 @@
-import {playSong} from "./handleButtonEvenets.js"
+import { playSong } from "./handleButtonEvenets.js";
 
 class Card {
-  constructor(title, artist, lyrics, mp3) {
-    this.title = title
-    this.artist = artist
-    this.mp3 = mp3
-    this.lyrics = lyrics
+  constructor(title, artist, lyrics, mp3, liked) {
+    this.title = title;
+    this.artist = artist;
+    this.mp3 = mp3;
+    this.lyrics = lyrics;
+    this._liked = liked;
   }
 
   createCard() {
-     const ulEl =  document.querySelector('.music_list')
-    this.liEl = document.createElement('li')
-    this.liEl.classList.add('music_item')
+    const ulEl = document.querySelector(".music_list");
+    this.liEl = document.createElement("li");
+    this.liEl.classList.add("music_item");
 
     this.liEl.innerHTML = `
           <div class="music_card">
@@ -31,51 +32,63 @@ class Card {
                 <use href="./img/sprite.svg#quatation-icon"></use>
               </svg>
              
-              <audio class="music_card-audio visually-hidden">
+              <audio class="music_card-audio " controls>
                 <source src="${this.mp3}" type="audio/mpeg">
                 Your browser does not support the audio element.
               </audio>
-          </div>
-    `
+             <label class="music_card-likedLabel">
+                <input class="music_card-likedCheck visually-hidden" type="checkbox">
+                <svg
+                  class="music_card-likedIcon"
+                  width="20"
+                  height="20"
+                  aria-hidden="true"
+                >
+                  <use href="./img/sprite.svg#web-like"></use>
+                </svg>
+              </label>
+    `;
 
-    ulEl.append(this.liEl)
-    
+    ulEl.append(this.liEl);
   }
-
 }
 
 export async function renderCards() {
-   const ulEl =  document.querySelector('.music_list')
-   const searchInput = document.querySelector(".header_menu-inputSearch")
+  const ulEl = document.querySelector(".music_list");
+  const searchInput = document.querySelector(".header_menu-inputSearch");
 
-   const songsList = await fetch("./js/data.json")
-  const songsData = await songsList.json()
+  const songsList = await fetch("./js/data.json");
+  const songsData = await songsList.json();
 
-  ulEl.innerHTML = ""
+  ulEl.innerHTML = "";
 
+  searchInput.addEventListener("input", function (e) {
+    ulEl.innerHTML = "";
 
-   searchInput.addEventListener("input", function(e) {
-    ulEl.innerHTML = ""
-
-    const filtered = songsData.filter((el) => el.title.trim().toLowerCase().includes(searchInput.value.trim().toLowerCase()))
-    doRender(filtered)
-    playSong()
-   })
-   
-
-  
+    const filtered = songsData.filter((el) =>
+      el.title
+        .trim()
+        .toLowerCase()
+        .includes(searchInput.value.trim().toLowerCase())
+    );
+    doRender(filtered);
+    playSong();
+  });
 
   function doRender(list) {
     list.forEach((song) => {
-    const card =  new Card(song.title, song.artist, song.lyrics, song.mp3)
-    card.createCard()
-  })
+      const card = new Card(
+        song.title,
+        song.artist,
+        song.lyrics,
+        song.mp3,
+        song.liked
+      );
+      card.createCard();
+    });
   }
 
-  doRender(songsData)
-  
+  doRender(songsData);
 
-  playSong()
-
+  playSong();
 }
-
